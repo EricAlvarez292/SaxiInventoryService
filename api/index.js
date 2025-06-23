a11c52efc89ea61387d5cd703ca695c6b70c16e3
AzureAD\EricAlvarez <eric@migo.io>
2025-06-24 01:33:34 +0800

    fix docs

--- api/index.js
+++ api/index.js
@@ -50,7 +50,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 const swaggerDocument = YAML.load(path.join(__dirname, '../public/swagger.yaml')); // adjust path as needed
-app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+const CSS_URL = 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css';
+app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {
+    customCss:
+        '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+    customCssUrl: CSS_URL
+}));
 
 // Listen on enviroment port or 5000 for local dev
 /* app.listen(port, () => console.log(`Listening on port ${port}`)) */
